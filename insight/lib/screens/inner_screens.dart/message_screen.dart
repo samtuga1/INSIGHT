633a34e1126453fe7b050829c11a11e796b16738
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-10-29 23:01:51 +0000

    update

--- insight/lib/screens/inner_screens.dart/message_screen.dart
+++ insight/lib/screens/inner_screens.dart/message_screen.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:insight/consts/global_methods.dart';
+import 'package:insight/providers/user_provider.dart';
 import 'package:insight/widgets/message_box.dart';
+import 'package:provider/provider.dart';
 
-class MessageScreen extends StatelessWidget {
+class MessageScreen extends StatefulWidget {
   static const routeName = '/message_screen';
   const MessageScreen({super.key});
 
   @override
+  State<MessageScreen> createState() => _MessageScreenState();
+}
+
+class _MessageScreenState extends State<MessageScreen> {
+  List<String> messages = [];
+  @override
   Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
     final theme = Theme.of(context);
     final isIos = theme.platform == TargetPlatform.iOS;
     return Scaffold(
@@ -58,26 +67,28 @@ class MessageScreen extends StatelessWidget {
                       child: ListView.builder(
                         padding: const EdgeInsets.only(top: 10),
                         physics: GlobalMethods.scrollPhysics(isIos),
-                        itemCount: 20,
+                        itemCount: messages.length,
                         itemBuilder: (context, index) => Align(
-                          alignment: index % 2 == 0
-                              ? Alignment.bottomRight
-                              : Alignment.bottomLeft,
+                          alignment: Alignment.bottomRight,
+                          // index % 2 == 0
+                          //     ? Alignment.bottomRight
+                          //     : Alignment.bottomLeft,
                           child: Container(
+                            margin: const EdgeInsets.only(bottom: 10),
                             padding: const EdgeInsets.all(12),
-                            decoration: BoxDecoration(
-                              color: const Color(0xff5669FF),
+                            decoration: const BoxDecoration(
+                              color: Color(0xff5669FF),
                               borderRadius: BorderRadius.only(
-                                bottomLeft:
-                                    Radius.circular(index % 2 == 0 ? 10 : 0),
-                                bottomRight:
-                                    Radius.circular(index % 2 == 0 ? 0 : 10),
-                                topLeft: const Radius.circular(10),
-                                topRight: const Radius.circular(10),
+                                bottomLeft: Radius.circular(10),
+                                // Radius.circular(index % 2 == 0 ? 10 : 0),
+                                // bottomRight:
+                                //     Radius.circular(index % 2 == 0 ? 0 : 10),
+                                topLeft: Radius.circular(10),
+                                topRight: Radius.circular(10),
                               ),
                             ),
                             child: Text(
-                              'Whatsap',
+                              messages[index],
                               style: theme.textTheme.bodyText1?.copyWith(
                                 color: Colors.white,
                               ),
@@ -89,7 +100,13 @@ class MessageScreen extends StatelessWidget {
                   ],
                 ),
               ),
-              const MessageBox(),
+              MessageBox(
+                onSubmit: (String value) {
+                  setState(() {
+                    messages.add(value);
+                  });
+                },
+              ),
             ],
           ),
         ),
