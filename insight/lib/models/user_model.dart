dc674f4d00fadc7717a61d079b984cdee1865bc8
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-10-29 23:01:26 +0000

    update

--- insight/lib/models/user_model.dart
+++ insight/lib/models/user_model.dart
@@ -1,10 +1,13 @@
 import 'package:insight/consts/enums.dart';
-import 'package:insight/models/pitch_model.dart';
 
 class UserModel {
   String? name;
-  String? userStatus;
+  UserStatus? userStatus;
   String? email;
-  List<PitchModel>? pitches;
-  List<PitchModel>? favPitches;
+
+  UserModel({
+    this.email,
+    this.name,
+    this.userStatus = UserStatus.businessOwner,
+  });
 }
