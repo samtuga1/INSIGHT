bc1e92fd1ae67327f2bbe5e0c2fc4bf16c3d7732
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-10-29 15:07:34 +0000

    update

--- insight/lib/consts/theme.dart
+++ insight/lib/consts/theme.dart
@@ -3,6 +3,22 @@ import 'package:flutter/material.dart';
 class AppTheme {
   static ThemeData appTheme() {
     return ThemeData(
+      accentColor: const Color(0xff769AF2),
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        backgroundColor: Colors.white,
+        unselectedLabelStyle: TextStyle(
+          fontWeight: FontWeight.w400,
+          fontSize: 16,
+          fontFamily: 'Gilroy',
+          color: Color(0xffD6DBDE),
+        ),
+        selectedLabelStyle: TextStyle(
+          fontWeight: FontWeight.w400,
+          fontSize: 16,
+          fontFamily: 'Gilroy',
+          color: Color(0xff5669FF),
+        ),
+      ),
       primaryColor: const Color(0xff5669FF),
       textTheme: const TextTheme(
         button: TextStyle(
