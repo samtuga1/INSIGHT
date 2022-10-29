a7e9ba9627abf28deee6da0856e63b461c0d9775
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-10-29 15:08:30 +0000

    update

--- insight/lib/main.dart
+++ insight/lib/main.dart
@@ -2,8 +2,13 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:insight/consts/theme.dart';
+import 'package:insight/providers/categories_provider.dart';
+import 'package:insight/screens/bottom_navigation.dart';
+import 'package:insight/screens/inner_screens.dart/pitch_detail_screen.dart';
 import 'package:insight/screens/onboarding/get_started_screen.dart';
 import 'package:insight/screens/onboarding/sign_up_screen.dart';
+import 'package:insight/screens/inner_screens.dart/message_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(
@@ -19,15 +24,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      useInheritedMediaQuery: true,
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
-      theme: AppTheme.appTheme(),
-      home: const GetStartedScreen(),
-      routes: {
-        SignUpScreen.routeName: (context) => const SignUpScreen(),
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<CategoryProvider>(
+          create: (context) => CategoryProvider(),
+        )
+      ],
+      child: MaterialApp(
+        useInheritedMediaQuery: true,
+        locale: DevicePreview.locale(context),
+        builder: DevicePreview.appBuilder,
+        theme: AppTheme.appTheme(),
+        home: const GetStartedScreen(),
+        routes: {
+          SignUpScreen.routeName: (context) => const SignUpScreen(),
+          NavigationScreen.routeName: (context) => const NavigationScreen(),
+          MessageScreen.routeName: (context) => const MessageScreen(),
+          PitchDetailScreen.routeName: (context) => const PitchDetailScreen(),
+        },
+      ),
     );
   }
 }
