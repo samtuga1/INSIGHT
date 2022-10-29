aa4fafd6bdf40a80ad93081cf08ca965f0167d10
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-10-29 16:38:41 +0000

    update

--- insight/lib/consts/global_methods.dart
+++ insight/lib/consts/global_methods.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class GlobalMethods {
   // GLOBAL BUTTON
@@ -29,5 +32,16 @@ class GlobalMethods {
     return isIos
         ? const BouncingScrollPhysics()
         : const AlwaysScrollableScrollPhysics();
+  }
+
+  static Future pickImage() async {
+    final ImagePicker _picker = ImagePicker();
+    // Pick an image
+    final image = await _picker.pickImage(source: ImageSource.gallery);
+
+    if (image == null) {
+      return;
+    }
+    return File(image.path);
   }
 }
